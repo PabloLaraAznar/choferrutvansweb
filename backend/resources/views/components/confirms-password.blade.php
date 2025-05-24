d7e1a2e0daa731c2225090cbe971cd9608d8ec90
RobertCCE <canulroberto36@gmail.com>
2025-05-24 17:23:09 -0600

    Perfil

--- backend/resources/views/components/confirms-password.blade.php
+++ backend/resources/views/components/confirms-password.blade.php
@@ -1,4 +1,9 @@
-@props(['title' => __('Confirm Password'), 'content' => __('For your security, please confirm your password to continue.'), 'button' => __('Confirm')])
+@props([
+    'title' => __('Confirm Password'), 
+    'content' => __('For your security, please confirm your password to continue.'), 
+    'button' => __('Confirmar'),  
+    'color' => 'primary'  {{-- Bootstrap color: primary, secondary, danger, etc. --}}
+])
 
 @php
     $confirmableId = md5($attributes->wire('then'));
@@ -10,37 +15,58 @@
     x-ref="span"
     x-on:click="$wire.startConfirmingPassword('{{ $confirmableId }}')"
     x-on:password-confirmed.window="setTimeout(() => $event.detail.id === '{{ $confirmableId }}' && $refs.span.dispatchEvent(new CustomEvent('then', { bubbles: false })), 250);"
+    class="text-{{ $color }} fw-semibold text-decoration-underline" 
+    style="cursor: pointer;"
 >
     {{ $slot }}
 </span>
 
 @once
-<x-dialog-modal wire:model.live="confirmingPassword">
+<x-dialog-modal wire:model.live="confirmingPassword" class="modal-dialog-centered">
     <x-slot name="title">
-        {{ $title }}
+        <h5 class="modal-title">
+            {{ $title }}
+        </h5>
     </x-slot>
 
     <x-slot name="content">
-        {{ $content }}
+        <p class="mb-3">
+            {{ $content }}
+        </p>
 
-        <div class="mt-4" x-data="{}" x-on:confirming-password.window="setTimeout(() => $refs.confirmable_password.focus(), 250)">
-            <x-input type="password" class="mt-1 block w-3/4" placeholder="{{ __('Password') }}" autocomplete="current-password"
-                        x-ref="confirmable_password"
-                        wire:model="confirmablePassword"
-                        wire:keydown.enter="confirmPassword" />
-
-            <x-input-error for="confirmable_password" class="mt-2" />
+        <div x-data="{}" x-on:confirming-password.window="setTimeout(() => $refs.confirmable_password.focus(), 250)">
+            <input 
+                type="password" 
+                class="form-control" 
+                placeholder="{{ __('Contraseña') }}" 
+                autocomplete="current-password"
+                x-ref="confirmable_password"
+                wire:model="confirmablePassword"
+                wire:keydown.enter="confirmPassword" 
+            />
+            <x-input-error for="confirmable_password" class="text-danger small mt-1" />
         </div>
     </x-slot>
 
-    <x-slot name="footer">
-        <x-secondary-button wire:click="stopConfirmingPassword" wire:loading.attr="disabled">
-            {{ __('Cancel') }}
-        </x-secondary-button>
+    <x-slot name="footer" class="d-flex justify-content-end gap-2">
+        <button 
+            type="button" 
+            wire:click="stopConfirmingPassword" 
+            wire:loading.attr="disabled" 
+            class="btn btn-danger"
+        >
+            {{ __('Cancelar') }}
+        </button>
 
-        <x-button class="ms-3" dusk="confirm-password-button" wire:click="confirmPassword" wire:loading.attr="disabled">
+        <button 
+            type="button" 
+            dusk="confirm-password-button" 
+            wire:click="confirmPassword" 
+            wire:loading.attr="disabled" 
+            class="btn btn-{{ $color }}"
+        >
             {{ $button }}
-        </x-button>
+        </button>
     </x-slot>
 </x-dialog-modal>
 @endonce
